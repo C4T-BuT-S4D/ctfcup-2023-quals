6816b6cedf54ae7e15a28e2761f5fd1761e912cf
user39043346 <143732806+user39043346@users.noreply.github.com>
2023-10-25 10:24:23 +0300

    more_heap: per-team

--- tasks/pwn/more_heap/prepare-public.sh
+++ tasks/pwn/more_heap/prepare-public.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 curdir=$(pwd)
@@ -8,9 +7,10 @@ pubtemp=$(mktemp -d)
 cp -R deploy $pubtemp/more_heap
 cd $pubtemp
 
-zip -r more_heap.zip more_heap
+sed -i '/image:/d' more_heap/docker-compose.yml
+
+zip -9 -r more_heap.zip more_heap
 
 cd $curdir
 mv $pubtemp/more_heap.zip public
-
 rm -rf $pubtemp
